01dcc59e98d0e1d0222f9d734a3b4d19120545c7
wwq-git <wwq921220@163.com>
2018-01-10 08:50:31 +0800

    ok

---
+++ GenSqlMap4Mybatis/src/main/java/com/wbhl/mapper/TbOrderMapper.java
@@ -0,0 +1,30 @@
+package com.wbhl.mapper;
+
+import com.wbhl.pojo.TbOrder;
+import com.wbhl.pojo.TbOrderExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface TbOrderMapper {
+    long countByExample(TbOrderExample example);
+
+    int deleteByExample(TbOrderExample example);
+
+    int deleteByPrimaryKey(String id);
+
+    int insert(TbOrder record);
+
+    int insertSelective(TbOrder record);
+
+    List<TbOrder> selectByExample(TbOrderExample example);
+
+    TbOrder selectByPrimaryKey(String id);
+
+    int updateByExampleSelective(@Param("record") TbOrder record, @Param("example") TbOrderExample example);
+
+    int updateByExample(@Param("record") TbOrder record, @Param("example") TbOrderExample example);
+
+    int updateByPrimaryKeySelective(TbOrder record);
+
+    int updateByPrimaryKey(TbOrder record);
+}
---
+++ GenSqlMap4Mybatis/src/test/java/cn/go/test/ComTest.java
@@ -0,0 +1,37 @@
+package cn.go.test;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.util.List;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import com.wbhl.mapper.TbOrderMapper;
+import com.wbhl.pojo.TbOrder;
+import com.wbhl.pojo.TbOrderExample;
+
+public class ComTest {
+
+	public static void main(String[] args) throws IOException {
+		TbOrderExample example = new TbOrderExample();
+		example.setPage(0L);
+		example.setRows(10L);
+		String resource = "SqlMapCOnfig.xml";
+		Reader reader = Resources.getResourceAsReader(resource);
+		SqlSessionFactory build = new SqlSessionFactoryBuilder().build(reader);
+		SqlSession openSession = build.openSession();
+		try {
+			TbOrderMapper mapper = openSession.getMapper(TbOrderMapper.class);
+			List<TbOrder> list = mapper.selectByExample(example);
+			System.out.println(list);
+			for (TbOrder TbOrder : list) {
+				System.out.println(TbOrder.getId() + "," + TbOrder.getAddress());
+			}
+		} finally {
+			openSession.close();
+		}
+	}
+}
